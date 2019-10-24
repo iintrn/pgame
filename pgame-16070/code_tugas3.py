dcf876500795227795fd435aedfa2f9a66bea762
iintrn <55899293+iintrn@users.noreply.github.com>
2019-10-24 10:27:25 +0700

    tugas3

---
+++ pgame-16070/code_tugas3.py
@@ -0,0 +1,9 @@
+# Latihan Buat Variabel : 
+nama = "Nama Saya 	: Iin Tarno" 
+print (nama)
+npm = "NPM 		: 121055520116070" 
+print (npm)
+semester = "Semester 	: 7 (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
